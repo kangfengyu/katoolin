2e58f269c313c90e7268f6e734ceb89a7954dc37
kangfengyu <131873884+kangfengyu@users.noreply.github.com>
2023-05-07 17:58:23 +0800

    Update katoolin.py

--- katoolin.py
+++ katoolin.py
@@ -51,16 +51,16 @@ def main():
 					repo = raw_input("\033[1;32mWhat do you want to do ?> \033[1;m")
 					if repo == "1":
 						cmd1 = os.system("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6")
-						cmd2 = os.system("echo '# Kali linux repositories | Added by Katoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list")
+						cmd2 = os.system("echo '# Kali linux repositories | Added by Katoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /data/data/com.termux/files/etc/apt/sources.list")
 					elif repo == "2":
 						cmd3 = os.system("apt-get update -m")
 					elif repo == "3":
-						infile = "/etc/apt/sources.list"
-						outfile = "/etc/apt/sources.list"
+						infile = "/data/data/com.termux/files/etc/apt/sources.list"
+						outfile = "/data/data/com.termux/files/etc/apt/sources.list"
 
 						delete_list = ["# Kali linux repositories | Added by Katoolin\n", "deb http://http.kali.org/kali kali-rolling main contrib non-free\n"]
 						fin = open(infile)
-						os.remove("/etc/apt/sources.list")
+						os.remove("/data/data/com.termux/files/etc/apt/sources.list")
 						fout = open(outfile, "w+")
 						for line in fin:
 						    for word in delete_list:
@@ -74,7 +74,7 @@ def main():
 					elif repo == "gohome":
 						inicio1()
 					elif repo == "4":
-						file = open('/etc/apt/sources.list', 'r')
+						file = open('/data/data/com.termux/files/etc/apt/sources.list', 'r')
 
 						print (file.read())
 
